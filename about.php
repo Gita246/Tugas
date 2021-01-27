822f07231269fdc3c6bdb193f200b931c571a5f4
Gita246 <78072103+Gita246@users.noreply.github.com>
2021-01-27 03:35:36 -0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>fresh</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout inner_page">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="index.html"><img src="images/logo.png" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item ">
+                                 <a class="nav-link" href="index.php">Beranda</a>
+                              </li>
+                              <li class="nav-item active">
+                                 <a class="nav-link" href="about.php">Tentang</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="juice.php">Cara Membuat</a>
+                              </li>
+                                                            
+                              <!--<li class="nav-item d_none">
+                                 <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                              </li>-->
+                           </ul>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!-- end header inner -->
+      <!-- end header -->
+      <!-- about -->
+      <div  class="about">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-7">
+                  <div class="about_img">
+                     <figure><img src="images/our1.png" alt="#" /></figure>
+                  </div>
+               </div>
+               <div class="col-md-5">
+                  <div class="titlepage">
+                     <h2>Jus Jeruk</h2>
+                     <p>Sari buah jeruk yang banyak mengandung vitamin C sangat baik karena selain menstimulasi sistem kekebalan tubuh, juga menghilangkan sumbatan lendir di tenggorokan, rongga hidung, paru-paru dan perut. Berguna pula untuk membersihkan liver dan menghilangkan rasa sakit di tubuh akibat influenza. Campuran sari jeruk nipis dan madu sangat berkhasiat menyembuhkan radang tenggorokan dan amandel. Bagi mereka yang memiliki gangguan lambung, tentu pilih buah jeruk yang tidak terlalu asam.</p>
+                     <h3>Kandungan Vitamin</h3>
+                        <p>Mengandung vitamin A, B1, B2, dan C.
+                           Mengandung anti kanker.
+                           Mencegah dan mengobati beragam penyakit dan gangguan kesehatan seperti mengobati sariawan dan menurunkan resiko terkena kardiovaskuler, kanker, dan katarak.
+                        <p>Fungsi utama dari jus jeruk adalah
+                           Memerangi infeksi,
+                           Memperkecil resiko stroke dan serangan jantung,
+                           Mengatasi flu dan demam.
+
+                           </p>
+                     <!--<a class="read_more" href="#">About More</a>-->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div  class="about">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-7">
+                  <div class="about_img">
+                     <figure><img src="images/our2.png" alt="#" /></figure>
+                  </div>
+               </div>
+               <div class="col-md-5">
+                  <div class="titlepage">
+                     <h2>Jus Leci</h2>
+                     <p>Jus leci memiliki kandungan berupa vitamin B kompleks antara lain thiamin, niasin, folat, serta riboflavin. Kandungan vitamin B kompleks tersebut dapat memperlancar sistem metabolisme tubuh untuk mencerna karbohidrat, protein, serta lemak.</p>
+                     <h3>Kandungan Vitamin</h3>
+                        <p>Mengandung vitamin C dan berbagai jenis vitamin B, seperti niacin, riboflavin, dan tiamin. Selain itu, buah manis ini juga dilengkapi dengan asam folat, kalium, dan tembaga.
+                        <p>Fungsi utama dari jus leci adalah mendukung fungsi tubuh dan meningkatkan sistem imun, Menjaga tekanan darah dan kesehatan jantung, Menjaga kesehatan sistem pencernaan, Menjaga kadar gula darah dan cocok untuk diet,dan Berpotensi mencegah berbagai penyakit kronis.
+                          
+                        </p>
+                     <!--<a class="read_more" href="#">About More</a>-->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div  class="about">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-7">
+                  <div class="about_img">
+                     <figure><img src="images/our4.png" alt="#" /></figure>
+                  </div>
+               </div>
+               <div class="col-md-5">
+                  <div class="titlepage">
+                     <h2>Jus Apel</h2>
+                     <p>Kombinasi kandungan garam mineral dan pektin dalam apel, serta kandungan asam oksalik pada bayam membentuk substansi unik yang memenuhi dinding-dinding usus dan melalui gerakan kimia ang kuat tapi aman “melepaskan” kotoran yang ada di usus besar yang telah mengendap berhari-hari, berbulan-bulan atau bahkan bertahun-tahun. Kandungan zat pektin dalam apel juga mampu menurunkan kadar kolesterol dan triglycerides yang mengganggu fungsi jantung.</p>
+                     <h3>Kandungan Vitamin</h3>
+                        <p>Mengandung vitamin A, B dan C.
+                           Menurunkan kadar kolesterol dalam darah.
+                           Menjadi zat antikanker,
+                           Mengurangi nafsu makan yang terlalu besar,
+                           Mengatasi gangguan lambung,
+                           Mengatasi anemia,
+                           Mengatasi  disentri,
+                           Mengatasi penyakit jantung,
+                           Mengatasi  rematik,
+                           Nengatasi  dan gangguan mata,
+                           Melindungi tubuh dari sindrom metabolik,
+                           Dapat meningkatkan daya tahan tubuh,
+                           Merawat gigi,dan
+                           Merawat kulit
+                        <p>Fungsi utama dari jus leci adalah:
+                           Mengurangi nafsu makan,
+                           Mengendalikan tekanan darah dan kadar gula darah, dan
+                           Pembersihan racun dalam usus
+                        </p>
+                     <!--<a class="read_more" href="#">About More</a>-->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <div  class="about">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-7">
+                  <div class="about_img">
+                     <figure><img src="images/our3.png" alt="#" /></figure>
+                  </div>
+               </div>
+               <div class="col-md-5">
+                  <div class="titlepage">
+                     <h2>Jus Delima</h2>
+                     <p>Di Irak dan Iran, jus delima yang dibuat kumur terlebih dahulu sebelum diminum membantu membersihkan mulut dan gigi, serta mencegah infeksi sehingga membantu menghilangkan bau mulut yang tidak sedap. Memakan dengan perlahan-lahan buah delima dan mengeluarkan bijinya dapat menjernihkan suara yang serak dan menghindari kekeringan tenggorokan. Manfaat lainnya, kandungan zat tanin dalam buah delima dapat membius cacing gelang, cacing kremi dan cacing pita dalam usus sehingga mereka dapat dikeluarkan melalui air besar. Cara ini sudah biasa digunakan oleh penduduk Mesir dan Vietnam.</p>
+                     <h3>Kandungan Vitamin</h3>
+                        <p>Mengandung vitamin B1, B2, B3, dan B6, kolin, vitamin E, serta vitamin K. Buah delima jug diperkaya dengan zat besi, zinc, tembaga dan selenium.
+                        <p>Fungsi utama dari jus delima adalah
+                         Mengikis kanker
+                        </p>
+                     <!--<a class="read_more" href="#">About More</a>--->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div  class="about">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-7">
+                  <div class="about_img">
+                     <figure><img src="images/our5.png" alt="#" /></figure>
+                  </div>
+               </div>
+               <div class="col-md-5">
+                  <div class="titlepage">
+                     <h2>Jus Nanas</h2>
+                     <p>Enzim bromealin dalam nanas melarutkan lendir yang sangat kental dalam sistem pencernaan sehingga juga dapat menghancurkan bisul bila ada. Masakan yang dibuat dengan 250 gram nanas yang diiris-iris, 60 gram cincangan daging ayam dan lada secukupnya yang kemudian digoreng dapat mengatasi penyakit darah rendah dengan gejala lemasnya kaki dan tangan.</p>
+                    <h3>Kandungan Vitamin</h3>
+                        <p>Mengandung vitamin B dan C.
+                        <p> Fungsi utama dari jus nanas yaitu : 
+                           Dapat mencegah resiko terkena serangan jantung dan stroke
+                           Dapat mengobati beragam penyakit dan gangguan kesehatan seperti menyembuhkan infeksi pada saluran pencernaan.</p>
+                     <!--<a class="read_more" href="#">About More</a>-->
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <!-- end about -->
+      <!--  footer -->
+      <footer id="contact">
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>fresh</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="index.html"><img src="images/logo.png" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item active">
+                                 <a class="nav-link" href="index.php">Beranda</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="about.php">Tentang</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="juice.php">Cara Membuat</a>
+                              </li>
+                              
+                              <!--<li class="nav-item d_none">
+                                 <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                              </li>-->
+                           </ul>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!-- end header -->
+
+      <!-- banner -->
+      <section class="banner_main">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="test_box">
+                     <div class="text-bg">
+                        <h1><span> Manfaat Jus</span> <br>Bagi kesehatan</h1>
+                        <p>Sari buah atau Jus adalah cairan yang terdapat secara alami dalam buah-buahan. Sari buah populer dikonsumsi manusia sebagai minuman. Biasanya dibuat dengan cara memblender buah-buahan bersama sedikit air dan takaran gula yang sesuai.</p>
+                        <!--<a class="read_more" href="#">Read More</a>-->
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div id="banner1" class="carousel slide banner_Carousel " data-ride="carousel">
+                     <ol class="carousel-indicators">
+                        <li data-target="#banner1" data-slide-to="0" class="active"></li>
+                        <li data-target="#banner1" data-slide-to="1"></li>
+                        <li data-target="#banner1" data-slide-to="2"></li>
+                        <li data-target="#banner1" data-slide-to="3"></li>
+                     </ol>
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                           <div class="container">
+                              <div class="carousel-caption ">
+                                 <div class="row">
+                                    <div class="col-md-12">
+                                       <div class="banner_img">
+                                          <figure><img src="images/banner_img.png" alt="#"/></figure>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="row">
+                                    <div class="col-md-12">
+                                       <div class="con_top">
+                                          <figure><img src="images/con_top.png" alt="#"/></figure>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="row">
+                                    <div class="col-md-12">
+                                       <div class="about.png">
+                                          <figure><img src="images/about.png" alt="#"/></figure>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="row">
+                                    <div class="col-md-12">
+                                       <div class="banner_img">
+                                          <figure><img src="images/banner_img.png" alt="#"/></figure>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <a class="carousel-control-prev" href="#banner1" role="button" data-slide="prev">
+                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Previous</span>
+                     </a>
+                     <a class="carousel-control-next" href="#banner1" role="button" data-slide="next">
+                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Next</span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end banner -->
+      <!-- about -->
+      <div  class="about">
+         <div class="container">
+            <div class="row d_flex">
+               
+            </div>
+         </div>
+      </div>
+      <!-- end Our Juices  section -->
+      
+      <!-- testimonial -->
+      <div class="testimonial">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Manfaat Jus</h2>
+                     <p>  Meningkatkan daya tahan tubuh </p>
+                     <p>  Menurunkan kadar kolesterol </p>
+                     <p>  Melancarkan proses pencernaan </p>
+                     <p>  Sebagai Anti Oksidan dan Anti Kanker </p>
+                     <p>  Mempercepat Proses Penyembuhan </p>
+                     <p>  Membuat Awet Muda</p>
+                  </div>
+               </div>
+            </div>
+            
+            </div>
+         </div>
+      </div>
+      <!-- end testimonial -->
+ 
+      <!--  footer -->
+      <footer id="contact">
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
+</html>
+
